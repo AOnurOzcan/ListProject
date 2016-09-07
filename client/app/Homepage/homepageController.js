294eae2e62ee295dc54adc45672e2e6ce8319647
Abdullah Onur Özcan <abdullah.ozcan1@ogr.sakarya.edu.tr>
2016-09-07 17:46:30 +0300

    HomeController'a test verisi eklendi.

--- client/app/Homepage/homepageController.js
+++ client/app/Homepage/homepageController.js
@@ -1,6 +1,118 @@
 angular.module('homepage')
 
-  .controller('homepageController', ['homepageService', function (homepageService) {
-    console.log("hi");
-    homepageService.query();
-  }]);
+    .controller('homepageController', ['homepageService', '$scope', function (homepageService, $scope) {
+        $scope.categories = ['Teknoloji', 'Dizi', 'Film', 'Oyun', 'Siyaset', 'Müzik', 'Spor', 'Bilim', 'Sosyal Medya'];
+        $scope.lists = [
+            {
+                title: "List 1",
+                items: [
+                    {
+                        name: "Item 1",
+                        likes: 10,
+                        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean semper ultricies est. Suspendisse tortor orci, imperdiet in metus in, ullamcorper auctor leo. Fusce et dapibus justo."
+                    },
+                    {
+                        name: "Item 2",
+                        likes: 13,
+                        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean semper ultricies est. Suspendisse tortor orci, imperdiet in metus in, ullamcorper auctor leo. Fusce et dapibus justo."
+                    }
+                ]
+            },
+            {
+                title: "List 2",
+                items: [{
+                    name: "Item 1",
+                    likes: 2,
+                    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean semper ultricies est. Suspendisse tortor orci, imperdiet in metus in, ullamcorper auctor leo. Fusce et dapibus justo."
+                },{
+                    name: "Item 1",
+                    likes: 10,
+                    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean semper ultricies est. Suspendisse tortor orci, imperdiet in metus in, ullamcorper auctor leo. Fusce et dapibus justo."
+                },
+                    {
+                        name: "Item 2",
+                        likes: 13,
+                        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean semper ultricies est. Suspendisse tortor orci, imperdiet in metus in, ullamcorper auctor leo. Fusce et dapibus justo."
+                    },{
+                        name: "Item 1",
+                        likes: 10,
+                        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean semper ultricies est. Suspendisse tortor orci, imperdiet in metus in, ullamcorper auctor leo. Fusce et dapibus justo."
+                    },
+                    {
+                        name: "Item 2",
+                        likes: 13,
+                        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean semper ultricies est. Suspendisse tortor orci, imperdiet in metus in, ullamcorper auctor leo. Fusce et dapibus justo."
+                    }]
+            },
+            {
+                title: "List 3",
+                items: [{
+                    name: "Item 1",
+                    likes: 60,
+                    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean semper ultricies est. Suspendisse tortor orci, imperdiet in metus in, ullamcorper auctor leo. Fusce et dapibus justo."
+                },{
+                    name: "Item 1",
+                    likes: 10,
+                    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean semper ultricies est. Suspendisse tortor orci, imperdiet in metus in, ullamcorper auctor leo. Fusce et dapibus justo."
+                },
+                    {
+                        name: "Item 2",
+                        likes: 13,
+                        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean semper ultricies est. Suspendisse tortor orci, imperdiet in metus in, ullamcorper auctor leo. Fusce et dapibus justo."
+                    },{
+                        name: "Item 1",
+                        likes: 10,
+                        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean semper ultricies est. Suspendisse tortor orci, imperdiet in metus in, ullamcorper auctor leo. Fusce et dapibus justo."
+                    },
+                    {
+                        name: "Item 2",
+                        likes: 13,
+                        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean semper ultricies est. Suspendisse tortor orci, imperdiet in metus in, ullamcorper auctor leo. Fusce et dapibus justo."
+                    },{
+                        name: "Item 1",
+                        likes: 10,
+                        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean semper ultricies est. Suspendisse tortor orci, imperdiet in metus in, ullamcorper auctor leo. Fusce et dapibus justo."
+                    },
+                    {
+                        name: "Item 2",
+                        likes: 13,
+                        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean semper ultricies est. Suspendisse tortor orci, imperdiet in metus in, ullamcorper auctor leo. Fusce et dapibus justo."
+                    }]
+            },
+            {
+                title: "List 4",
+                items: [{
+                    name: "Item 1",
+                    likes: 40,
+                    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean semper ultricies est. Suspendisse tortor orci, imperdiet in metus in, ullamcorper auctor leo. Fusce et dapibus justo."
+                },{
+                    name: "Item 1",
+                    likes: 10,
+                    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean semper ultricies est. Suspendisse tortor orci, imperdiet in metus in, ullamcorper auctor leo. Fusce et dapibus justo."
+                },
+                    {
+                        name: "Item 2",
+                        likes: 13,
+                        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean semper ultricies est. Suspendisse tortor orci, imperdiet in metus in, ullamcorper auctor leo. Fusce et dapibus justo."
+                    },{
+                        name: "Item 1",
+                        likes: 10,
+                        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean semper ultricies est. Suspendisse tortor orci, imperdiet in metus in, ullamcorper auctor leo. Fusce et dapibus justo."
+                    },
+                    {
+                        name: "Item 2",
+                        likes: 13,
+                        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean semper ultricies est. Suspendisse tortor orci, imperdiet in metus in, ullamcorper auctor leo. Fusce et dapibus justo."
+                    },{
+                        name: "Item 1",
+                        likes: 10,
+                        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean semper ultricies est. Suspendisse tortor orci, imperdiet in metus in, ullamcorper auctor leo. Fusce et dapibus justo."
+                    },
+                    {
+                        name: "Item 2",
+                        likes: 13,
+                        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean semper ultricies est. Suspendisse tortor orci, imperdiet in metus in, ullamcorper auctor leo. Fusce et dapibus justo."
+                    }]
+            }
+        ];
+    }]);
